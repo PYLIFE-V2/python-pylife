e7badef95224385622c853aa771ef58881599d37
David Sn <divad.nnamtdeis@gmail.com>
2023-03-27 18:30:19 +0200

    refactor(client): Refactor fetch method and add json serializer

--- pylife_api/client.py
+++ pylife_api/client.py
@@ -5,9 +5,9 @@ import aiohttp
 from pydantic import parse_obj_as
 
 try:
-    from rapidjson import loads
+    import rapidjson as json
 except ImportError:
-    from json import loads
+    import json
 
 from .models import House, Organization, Player
 
@@ -20,45 +20,43 @@ class PylifeAPIClient:
         self.auth_token = auth_token
         self.session = aiohttp.ClientSession(
             base_url="https://panel.pylife-rpg.pl/",
+            json_serialize=json.dumps,
             headers={
                 "Content-Type": "application/json",
                 "User-Agent": f"pylife-api/2.0 (Python {python_version()}; aiohttp {aiohttp.__version__})",
-            }
+            },
         )
 
-    async def fetch(self, url) -> Any:
-        async with self.session.get(url) as resp:
+    async def fetch(self, method: str, url: str, **kwargs) -> Any:
+        async with self.session.request(method=method, url=url, **kwargs) as resp:
             resp.raise_for_status()
-            return await resp.json(loads=loads)
+            return await resp.json(loads=json.loads)
 
     async def get_house(self, house_id: int) -> House:
-        json_data = await self.fetch(f"/api/domy/{house_id}")
+        json_data = await self.fetch("GET", f"/api/domy/{house_id}")
         return House.parse_obj(json_data)
 
     async def get_houses(self) -> List[House]:
-        json_data = await self.fetch("/api/domy")
+        json_data = await self.fetch("GET", "/api/domy")
         return parse_obj_as(List[House], json_data)
 
     async def get_organization(self, organization_id: int) -> Organization:
-        json_data = await self.fetch(f"/api/organizacje/{organization_id}")
+        json_data = await self.fetch("GET", f"/api/organizacje/{organization_id}")
         return Organization.parse_obj(json_data)
 
     async def get_organizations(self) -> List[Organization]:
-        json_data = await self.fetch("/api/organizacje")
+        json_data = await self.fetch("GET", "/api/organizacje")
         return parse_obj_as(List[Organization], json_data)
 
     async def get_player(self, player_id: int) -> Player:
-        json_data = await self.fetch(f"/api/gracz/{player_id}")
+        json_data = await self.fetch("GET", f"/api/gracz/{player_id}")
         return Player.parse_obj(json_data)
 
     async def get_players(self) -> List[Player]:
         if not self.auth_token:
             raise ValueError("No auth_token provided for authorization")
 
-        async with self.session.post("/api/gracze", json={"auth": self.auth_token}) as resp:
-            resp.raise_for_status()
-            json_data = await resp.json(loads=loads)
-
+        json_data = await self.fetch("POST", "/api/gracze", json={"auth": self.auth_token})
         return parse_obj_as(List[Player], json_data)
 
     async def close(self):
